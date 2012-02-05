ab665d44904f579ff57270010a7f78df32ba8e41
Francesco Freezone <cescofry@gmail.com>
2012-02-05 17:55:36 +0000

    add activity view

---
+++ ZFActivityView.h
@@ -0,0 +1,13 @@
+//
+//  ZFActivityView.h
+//  Solgar
+//
+//  Created by cescofry Freezone on 05/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZFActivityView : UIView
+
+@end
